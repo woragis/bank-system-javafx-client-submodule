c71db64da78a5bc9c9037e7b096c5dc70f9041a7
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-05 02:08:10 -0300

    Create password utils

---
+++ src/main/java/com/woragis/utils/PasswordUtils.java
@@ -0,0 +1,21 @@
+package com.woragis.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordUtils {
+
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256"); // or bcrypt for real apps
+            byte[] hash = md.digest(password.getBytes());
+            StringBuilder hex = new StringBuilder();
+            for (byte b : hash) {
+                hex.append(String.format("%02x", b));
+            }
+            return hex.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
+    }
+}
