38363ae73076345f51bf23bffb5c8880c1da2f3f
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-05 01:44:58 -0300

    Create stage

--- src/main/java/com/woragis/MainApp.java
+++ src/main/java/com/woragis/MainApp.java
@@ -2,24 +2,40 @@ package com.woragis;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
 
 public class MainApp extends Application {
     private static Stage stage;
 
     @Override
     public void start(@SuppressWarnings("exports") Stage s) throws IOException {
-        stage=s;
-        setRoot("primary","");
+        // stage=s;
+        // setRoot("primary","");
+        Stage stage = new Stage();
+
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        stage.setTitle("Woragis Bank");
+        stage.setScene(scene);
+        stage.setWidth(800);
+        stage.setHeight(800);
+        stage.setResizable(false);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("Press this Q");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+        stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
-        setRoot(fxml,stage.getTitle());
+        setRoot(fxml, stage.getTitle());
     }
 
     static void setRoot(String fxml, String title) throws IOException {
@@ -30,10 +46,9 @@ public class MainApp extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/"+fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
-
 
     public static void main(String[] args) {
         launch(args);
